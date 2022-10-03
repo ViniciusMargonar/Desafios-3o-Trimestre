2c6ef8ae7bda69da83f49571f9ed0bc44d1fc626
joa1-oliv <111815573+joa1-oliv@users.noreply.github.com>
2022-10-03 20:02:19 -0300

    Add files via upload

---
+++ 26-09-22/Arquivo 01 - Termo Ciencia Joao.sql
@@ -0,0 +1,8 @@
+/*
+
+Eu Joao Victor, estou ciente das regras da avaliacao trimestal e que a mesma será feita em equipe. Também afirmo conhecer que o conceito do trimestre terá 
+como base o desempenho demonstrado pelo grupo durante a execuçao das atividades. Atividades estas que poderao substituir a avaliação dissertativa.
+A equipe que se destacar, poderá, de acordo com as regras, escolher seu conceito.
+
+
+*/
